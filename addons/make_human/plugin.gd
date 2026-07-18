7b4c68a17198bcdc72bbd7df11ae326bd7a04b91
Hennadii Chernyshchyk <genaloner@gmail.com>
2026-07-18 03:04:58 +0300

    Add project settings

--- addons/make_human/plugin.gd
+++ addons/make_human/plugin.gd
@@ -1,6 +1,8 @@
 @tool
 extends EditorPlugin
 
+const ASSETS_DIR_SETTING := "make_human/general/assets"
+
 var _obj_importer := MakeHumanObjImporter.new()
 var _target_importer := MakeHumanTargetImporter.new()
 var _mhmat_importer := MakeHumanMhmatImporter.new()
@@ -8,6 +10,18 @@ var _mhclo_importer := MakeHumanMhcloImporter.new()
 
 
 func _enter_tree() -> void:
+	if not ProjectSettings.has_setting(ASSETS_DIR_SETTING):
+		ProjectSettings.set_setting(ASSETS_DIR_SETTING, "")
+
+	ProjectSettings.set_initial_value(ASSETS_DIR_SETTING, "")
+	ProjectSettings.add_property_info(
+		{
+			"name": ASSETS_DIR_SETTING,
+			"type": TYPE_STRING,
+			"hint": PROPERTY_HINT_DIR,
+		},
+	)
+
 	add_import_plugin(_obj_importer)
 	add_import_plugin(_target_importer)
 	add_import_plugin(_mhmat_importer)
