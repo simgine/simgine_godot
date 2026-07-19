9e9a2d31dd66bedffefadfc269a637e9b6afba84
Hennadii Chernyshchyk <genaloner@gmail.com>
2026-07-20 01:29:28 +0300

    Rework meshes generation

---
+++ addons/make_human/attachment_instance.gd
@@ -0,0 +1,40 @@
+@tool
+class_name MakeHumanAttachmentInstance
+extends MeshInstance3D
+
+@export var base: MakeHumanBase:
+	set = _set_base
+@export var attachment: MakeHumanAttachment:
+	set(value):
+		if attachment != value:
+			attachment = value
+			_update_mesh()
+
+
+func _ready() -> void:
+	_update_mesh()
+
+
+func _validate_property(property: Dictionary) -> void:
+	if property.name == "mesh":
+		# Hide entirely since it's constructed dynamically.
+		property.usage = PROPERTY_USAGE_NONE
+
+
+func _set_base(value: MakeHumanBase) -> void:
+	if base == value:
+		return
+
+	if base:
+		base.changed.disconnect(_update_mesh)
+
+	base = value
+
+	if base:
+		base.changed.connect(_update_mesh)
+
+	_update_mesh()
+
+
+func _update_mesh() -> void:
+	mesh = base.get_attachment_mesh(attachment) if base and attachment else null
