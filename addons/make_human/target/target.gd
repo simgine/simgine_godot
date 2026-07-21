29a111d31029191a2008b39b5637ffd1a1c268f9
Hennadii Chernyshchyk <genaloner@gmail.com>
2026-07-21 23:28:15 +0300

    Remove unused class

--- addons/make_human/target/target.gd
+++ addons/make_human/target/target.gd
@@ -6,13 +6,3 @@ extends Resource
 
 @export_storage var vertex_indices: PackedInt32Array
 @export_storage var offsets: PackedVector3Array
-
-## Helper to attach a file name.
-class Meta:
-	var path: String
-	var target: MakeHumanTarget
-
-
-	func _init(new_path: String, new_target: MakeHumanTarget) -> void:
-		path = new_path
-		target = new_target
