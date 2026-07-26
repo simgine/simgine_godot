befa6fca9032c328164445c673d8a4b8950429f4
Hennadii Chernyshchyk <genaloner@gmail.com>
2026-07-27 00:15:44 +0300

    Add newlines between fields with doc comments

--- addons/make_human/attachment/scale.gd
+++ addons/make_human/attachment/scale.gd
@@ -10,8 +10,10 @@ extends Resource
 ## Despite the name inherited from the `.mhclo` format, this does not
 ## necessarily identify the vertex with the smaller coordinate value.
 @export var min_vertex: int
+
 ## Index of the second body vertex.
 @export var max_vertex: int
+
 ## Values used to divide the distance between [member min_vertex] and [member max_vertex]
 ##
 ## Must not be zero.
