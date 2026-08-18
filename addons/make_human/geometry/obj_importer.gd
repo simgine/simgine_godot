91e1aae343e6e9f27284a7c435cb45a01527d9bd
Hennadii Chernyshchyk <genaloner@gmail.com>
2026-08-18 23:13:41 +0300

    Remove priority override

--- addons/make_human/geometry/obj_importer.gd
+++ addons/make_human/geometry/obj_importer.gd
@@ -33,10 +33,6 @@ func _get_preset_name(_preset_index: int) -> String:
 	return ""
 
 
-func _get_priority() -> float:
-	return 2.0 # Make it higher than the default OBJ importer.
-
-
 func _get_import_options(_path: String, _preset_index: int) -> Array[Dictionary]:
 	return []
 
